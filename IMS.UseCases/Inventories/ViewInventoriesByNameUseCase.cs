a238120e9ad1d7b639c03a581da14cb003fb87b0
Steve Shillitoe <s.shillitoe99@gmail.com>
2026-01-30 20:44:36 +0000

    Add project files.

---
+++ IMS.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases.Inventories
+{
+    public class ViewInventoriesByNameUseCase
+    {
+        private readonly IInventoryRepository inventoryRepository;
+        public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository)
+        {
+            this.inventoryRepository = inventoryRepository;
+
+        }
+        public async Task <IEnumerable<Inventory>> ExecuteAsync(string name = "")
+        {
+            return await inventoryRepository.GetInventoriesByNameAsync(name);
+        }
+    }
+}
---
+++ IMS.UseCases/PluginInterfaces/IInventoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IMS.CoreBusiness;
+
+namespace IMS.UseCases.PluginInterfaces
+{
+    public interface IInventoryRepository
+    {
+        Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name);
+    }
+}
