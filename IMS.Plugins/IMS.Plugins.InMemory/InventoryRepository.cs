e36193364716381283c90b6589be948362895fea
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2026-03-09 23:12:18 +0000

    Create in memory Inventory list

--- IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -10,10 +10,12 @@ namespace IMS.Plugins.InMemory
 		{
 			_inventories = new List<Inventory>();
 			{
-				_inventories.Add(new Inventory() { });
-				_inventories.Add(new Inventory() { });
-				_inventories.Add(new Inventory() { });
+				new Inventory {InventoryId=1, InventoryName="Bike Seat", Quantity=10, Price=2};
+				new Inventory {InventoryId = 2, InventoryName = "Bike Frame", Quantity = 10, Price = 15 };
+				new Inventory {InventoryId = 3, InventoryName = "Bike Wheel", Quantity = 20, Price = 8 };
+				new Inventory {InventoryId = 4, InventoryName = "Bike Pedals", Quantity = 20, Price = 1 };
 			}
+			;
 		}
 
 		public Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name)
