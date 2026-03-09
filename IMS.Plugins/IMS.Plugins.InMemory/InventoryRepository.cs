93cd463eb13293520a9d5eb743fd8f35a9df3cbb
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2026-03-09 22:47:19 +0000

    Add IMS.Plugins class library

---
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.Plugins.InMemory
+{
+	public class InventoryRepository : IInventoryRepository
+	{
+		private List<Inventory> _inventories;
+		public InventoryRepository()
+		{
+			_inventories = new List<Inventory>();
+			{
+				_inventories.Add(new Inventory() { });
+				_inventories.Add(new Inventory() { });
+				_inventories.Add(new Inventory() { });
+			}
+		}
+
+		public Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
