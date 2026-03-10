7a5a0085fc31dbd0263b741390290baec8101aa0
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2026-03-10 21:29:33 +0000

    Create Interface

--- IMS.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
+++ IMS.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
@@ -6,17 +6,17 @@ using IMS.UseCases.PluginInterfaces;
 
 namespace IMS.UseCases.Inventories
 {
-    public class ViewInventoriesByNameUseCase
-    {
-        private readonly IInventoryRepository inventoryRepository;
-        public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository)
-        {
-            this.inventoryRepository = inventoryRepository;
+	public class ViewInventoriesByNameUseCase : IViewInventoriesByNameUseCase
+	{
+		private readonly IInventoryRepository inventoryRepository;
+		public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository)
+		{
+			this.inventoryRepository = inventoryRepository;
 
-        }
-        public async Task <IEnumerable<Inventory>> ExecuteAsync(string name = "")
-        {
-            return await inventoryRepository.GetInventoriesByNameAsync(name);
-        }
-    }
+		}
+		public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "")
+		{
+			return await inventoryRepository.GetInventoriesByNameAsync(name);
+		}
+	}
 }
