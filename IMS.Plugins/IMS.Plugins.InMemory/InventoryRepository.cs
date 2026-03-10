7f540a9a6577b2afcc4d49273c5edb5b7e510398
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2026-03-10 21:15:05 +0000

    Fix typo

--- IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -8,12 +8,12 @@ namespace IMS.Plugins.InMemory
 		private List<Inventory> _inventories;
 		public InventoryRepository()
 		{
-			_inventories = new List<Inventory>();
+			_inventories = new List<Inventory>()
 			{
-				new Inventory {InventoryId=1, InventoryName="Bike Seat", Quantity=10, Price=2};
-				new Inventory {InventoryId = 2, InventoryName = "Bike Frame", Quantity = 10, Price = 15 };
-				new Inventory {InventoryId = 3, InventoryName = "Bike Wheel", Quantity = 20, Price = 8 };
-				new Inventory {InventoryId = 4, InventoryName = "Bike Pedals", Quantity = 20, Price = 1 };
+				new Inventory { InventoryId = 1, InventoryName = "Bike Seat", Quantity = 10, Price = 2 },
+				new Inventory { InventoryId = 2, InventoryName = "Bike Body", Quantity = 10, Price = 15 },
+				new Inventory { InventoryId = 3, InventoryName = "Bike Wheels", Quantity = 20, Price = 8 },
+				new Inventory { InventoryId = 4, InventoryName = "Bike Pedels", Quantity = 20, Price = 1 },
 			};
 		}
 
