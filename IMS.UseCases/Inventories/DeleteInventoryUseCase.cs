3422873c5ad433d8c8358e1d3d2a72ff77934e0a
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2026-03-23 12:47:32 +0000

    Add delete use case

---
+++ IMS.UseCases/Inventories/DeleteInventoryUseCase.cs
@@ -0,0 +1,19 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.Inventories.Interfaces;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases.Inventories
+{
+	public class DeleteInventoryUseCase : IDeleteInventoryUseCase
+	{
+		private readonly IInventoryRepository _inventoryRepository;
+		public DeleteInventoryUseCase(IInventoryRepository inventoryRepository)
+		{
+			this._inventoryRepository = inventoryRepository;
+		}
+		public async Task ExecuteAsync(int inventoryId)
+		{
+			await this._inventoryRepository.DeleteInventoryByIdAsync(inventoryId);
+		}
+	}
+}
