cec20817e7e18f513395f5d9bf8edf9ef50ced93
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2026-03-27 21:50:48 +0000

    Add Delete Product functionality

---
+++ IMS.UseCases/Products/DeleteProductUseCase.cs
@@ -0,0 +1,19 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.Products.Interfaces;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases.Products
+{
+	public class DeleteProductUseCase : IDeleteProductUseCase
+	{
+		private readonly IProductRepository _productRepository;
+		public DeleteProductUseCase(IProductRepository productRepository)
+		{
+			this._productRepository = productRepository;
+		}
+		public async Task ExecuteAsync(int productId)
+		{
+			await this._productRepository.DeleteProductByIdAsync(productId);
+		}
+	}
+}
