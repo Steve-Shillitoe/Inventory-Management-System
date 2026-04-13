dbc8e358c7054490535f7e16c0bf81fea2f72351
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2026-04-13 23:28:43 +0100

    Add model class

---
+++ IMS.WebApp/ViewModels/PurchaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.WebApp.ViewModels
+{
+	public class PurchaseViewModel
+	{
+		
+		[Required]
+		public string PONumber { get; set; } = string.Empty;
+
+		[Range(minimum:1, maximum: int.MaxValue, ErrorMessage = "Please select an inventory")]
+		public int InventoryId { get; set; }
+		[Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "Quantity has to be greater than or equal to 1")]
+		public int QuantityToPurchase { get; set; }
+
+		[Range(minimum: 0.00, maximum: double.MaxValue, ErrorMessage = "Price cannot be negative.")]
+		public double InventoryPrice { get; set; }
+	}
+}
